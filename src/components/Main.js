59220e11bdacd944dc68b309a8b01bdef027d580
zhuofanw <zhuofan_wei@outlook.com>
2018-08-21 20:10:38 -0700

    main init

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Main extends React.Component{
+    render(){
+        return(
+            <div className={"main"}>
+                <div>LEFT</div>
+                <div>RIGHT</div>
+            </div>
+        );
+    }
+}
