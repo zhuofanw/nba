844dac74c0f5de79a1c452efbabb1767b737eb5f
zhuofanw <zhuofan_wei@outlook.com>
2018-08-21 21:00:46 -0700

    updated state in main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,30 @@ import {Profile} from "./Profile"
 import {ShotChart} from "./ShotChart"
 
 export class Main extends React.Component{
-    componentDidMount(){
-        nba.findPlayer("Stephen Curry");
+    state = {
+        playerId: nba.findPlayer("Stephen Curry").playerId,
+        playerInfo: {}
+    }
+    componentDidMount() {
+        const playerId = nba.findPlayer("Stephen Curry").playerId;
+        nba.stats.playerInfo({PlayerID: playerId}).then((response) => {
+            //commonPlayerInfo[0]
+            //playerHeadlineStats[0]
+            const playerInfo = Object.assign(
+                {}, response.commonPlayerInfo[0], response.playerHeadlineStats[0]
+            );
 
+            this.setState({
+                playerInfo
+            });
+        })
     }
     render(){
+        console.log(this.state.playerInfo);
         return(
             <div className={"main"}>
                 <Profile/>
-                <ShotChart playerId={201939}/>
+                <ShotChart playerId={this.state.playerId}/>
             </div>
         );
     }
