ee492d7381e39f5f16f8fb9c3655507c75adc332
zhuofanw <zhuofan_wei@outlook.com>
2018-08-28 19:33:47 -0700

    add search bar -auto complete

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { AutoComplete , Input, Icon} from 'antd';
+import nba from 'nba';
+
+
 
 export class SearchBar extends React.Component {
-    render(){
-        return(
-            <div>
-                Search Bar
-            </div>
+    state = {
+        dataSource: [],
+    }
+
+    handleSearch = (value) => {
+        this.setState({
+            dataSource: !value ? [] : nba.searchPlayers(value).map(
+                ({fullName}) => fullName
+            ),
+        });
+    }
+
+    onSelect= (value) => {
+        console.log('onSelect', value);
+    }
+
+    render() {
+        const { dataSource } = this.state;
+        return (
+            <AutoComplete
+                className="search-bar"
+                dataSource={dataSource}
+                size="large"
+                onSelect={this.onSelect}
+                onSearch={this.handleSearch}
+                placeholder="Search NBA player"
+            >
+                <Input suffix={<Icon type="search" className="certain-category-icon" />} />
+            </AutoComplete>
         );
     }
 }
