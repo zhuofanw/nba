0aafd1ca2d407efc5fcb1c6c4ca5a15f8643f649
zhuofanw <zhuofanw@sfu.ca>
2019-04-12 00:57:54 -0700

    Updates

---
+++ precache-manifest.fc040afabefec78edcca0ab0fc96fcf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9470e2d5c6c7f425edc8e9d8baebdb05",
+    "url": "/nba/static/media/logo.9470e2d5.svg"
+  },
+  {
+    "revision": "ac17c598394098d8b6b8",
+    "url": "/nba/static/js/runtime~main.29a9a1d4.js"
+  },
+  {
+    "revision": "7d181d93c8191d9cd244",
+    "url": "/nba/static/js/main.3fe27834.chunk.js"
+  },
+  {
+    "revision": "c03ad4e0df94bc152aaa",
+    "url": "/nba/static/js/2.2c564359.chunk.js"
+  },
+  {
+    "revision": "7d181d93c8191d9cd244",
+    "url": "/nba/static/css/main.85aad4bb.chunk.css"
+  },
+  {
+    "revision": "c03ad4e0df94bc152aaa",
+    "url": "/nba/static/css/2.5ec74ece.chunk.css"
+  },
+  {
+    "revision": "896ba8f4edca40ea7203b58a77d997fe",
+    "url": "/nba/index.html"
+  }
+];
